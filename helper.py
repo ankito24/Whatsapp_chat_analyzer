a297ac5bbfe34542f5f511b50b2667babc729a72
ankito24 <ankitiitk28@gmail.com>
2026-07-01 01:12:43 +0530

    cleaned the messages by removing the stop_words

--- helper.py
+++ helper.py
@@ -36,22 +36,41 @@ def create_wordcloud(selected_user,df):
     f = open('stop_hinglish.txt', 'r')
     stop_words = f.read()
 
-    #     # fetch no. of messages
-    #     num_messages = df.shape[0]
-    #
-    #     # number of words
-    #     words = []
-    #     for message in df['message']:
-    #         words.extend(message.split())
-    #
-    #     return num_messages,len(words)
-    #
-    # else:
-    #
-    #     new_df= df[df["user"]==  selected_user ]
-    #     num_messages = new_df.shape[0]                 ## shape[0] mean no. of row in the selected user dataframe
-    #     words = []
-    #     for message in new_df['message']:
-    #         words.extend(message.split())
-    #
-    #     return num_messages,len(words)
+    if selected_user != "Overall":
+        df = df[df["user"] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']  # temp ha no msgs of "group_notification"
+    temp = temp[temp['message'] != '<Media omitted>\n']  # temp has no msgs like "media_omitted"
+
+    def remove_stop_words(message):
+        y=[]
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+
+    wc= WordCloud(width = 500, height = 500, min_font_size = 10,background_color = "white")
+    temp['message']= temp['message'].apply(remove_stop_words)
+    df_wc=wc.generate(temp['message'].str.cat(sep=' '))
+    return df_wc
+
+def most_common_words(selected_user,df):
+
+    f=open('stop_hinglish.txt','r')
+    stop_words= f.read()
+
+    if selected_user != "Overall":
+        df = df[df["user"] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']  # temp ha no msgs of "group_notification"
+    temp = temp[temp['message'] != '<Media omitted>\n']  # temp has no msgs like "media_omitted"
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df= pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
